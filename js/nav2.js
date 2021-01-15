98989dbcccdf276cebe0a34972ca096f20b10910
Mario Parrish <marioparrish@Marios-MBP-2.attlocal.net>
2021-01-15 13:18:51 -0800

    Changing Nav from Jquery to Javascript

--- js/nav2.js
+++ js/nav2.js
@@ -1,45 +1,25 @@
-/**************************
- * Display active on scroll
- * ************************/
-// $(document).ready(function () {
-
-//     var scrollLink = $('.nav-button')
-//     $(window).scroll(function () {
-//         var scrollbarLocation = $(this).scrollTop();
-
-//         scrollLink.each(function() {
-            
-//             var sectionOffset = $(this.hash).offset().top
-
-//             if(sectionOffset <= scrollbarLocation) {
-//                 $(this).parent().siblings().removeClass('active');
-//                 $(this).parent().addClass('active');
-//             }
-//         })
-//     })
-// })
-
-
+//Display active on scroll
 function onPageLoad() {
 	var scrollLinks = Array.from(document.getElementsByClassName("nav-button"));
+	
 	window.onscroll = function() {
 		scrollLinks.forEach(ele => {
 			var id = ele.getAttribute("href");
+
 			var sectionOffset = document.getElementById(id.substring(1)).getBoundingClientRect().y;
+			
 			if(sectionOffset <= 0){
 				for(var i = 0; i < scrollLinks.length; i++){
 					scrollLinks[i].parentNode.classList.remove("active")
 				}
+				
 				ele.parentElement.classList.add("active");
 			}
 		})
 	}
 }
 
-
-/************************** 
- * Display active on click
- * ************************/
+//Display active on click
 $(document).ready(function () {
     $('li').on('click', function () {
         $(this).siblings().removeClass('active');
@@ -50,13 +30,24 @@ $(document).ready(function () {
 /*************
     Hamburger
     *********/
-function openBurger() {
+   function openBurger() {
     console.log("Open")
     var dropDown = document.getElementById("burger");
-    dropDown.classList.add("open"); 
+    dropDown.classList.add("open");
 }
 function closeBurger() {
     console.log("Close")
     var dropDown = document.getElementById("burger");
     dropDown.classList.remove("open");
+}
+
+/*************
+    Check if mobile or desktop
+    *********/
+   function checkAboutUs() {
+    console.log("in about us")
+    var desktopVersion = document.getElementById("about-us");
+	if(desktopVersion.attributes.display = "none"){
+		return true
+	}
 }
