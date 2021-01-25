5cc9b2b1ce249a987639a4bd8feefa1e37aa8f99
Mario Parrish <marioparrish@Marios-MBP-2.attlocal.net>
2021-01-25 11:30:51 -0800

    Updated Javascript for Nav

--- js/nav.js
+++ js/nav.js
@@ -1,31 +1,16 @@
 /**************************
  * Display active on scroll
  * ************************/
-// $(document).ready(function () {
-
-//     var scrollLink = $('.nav-button')
-//     $(window).scroll(function () {
-//         var scrollbarLocation = $(this).scrollTop();
-
-//         scrollLink.each(function() {
-            
-//             var sectionOffset = $(this.hash).offset().top
-
-//             if(sectionOffset <= scrollbarLocation) {
-//                 $(this).parent().siblings().removeClass('active');
-//                 $(this).parent().addClass('active');
-//             }
-//         })
-//     })
-// })
-
-
 function onPageLoad() {
 	var scrollLinks = Array.from(document.getElementsByClassName("nav-button"));
+	// console.log(scrollLinks)
 	window.onscroll = function() {
 		scrollLinks.forEach(ele => {
 			var id = ele.getAttribute("href");
 			var sectionOffset = document.getElementById(id.substring(1)).getBoundingClientRect().y;
+			if (id.includes("mobile")){
+				return
+			}
 			if(sectionOffset <= 0){
 				for(var i = 0; i < scrollLinks.length; i++){
 					scrollLinks[i].parentNode.classList.remove("active")
@@ -40,12 +25,18 @@ function onPageLoad() {
 /************************** 
  * Display active on click
  * ************************/
-$(document).ready(function () {
-    $('li').on('click', function () {
-        $(this).siblings().removeClass('active');
-        $(this).addClass('active');
-    })
-})
+function navOnClick() {
+	var btns = document.getElementsByClassName('btn-wrapper');
+	Array.from(btns).forEach(btn => {
+		btn.addEventListener('click', () => {
+			var selected = Array.from(document.getElementsByClassName("active"));
+			selected[0].className = selected[0].className.replace(" active", "");
+			console.log(selected[0].className)
+			btn.className += " active";
+		})
+	})
+	
+}
 
 /*************
     Hamburger
